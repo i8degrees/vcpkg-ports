002ab59e5e7f45c14f64c5c963ac4a90ebcea7f4
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-07-12 11:55:47 -0500

    Update ports/librocket-130
    This is the original release that we forked from upstream and
    are now ready to replace this variant with the official upstream sources,
    less & except vcpkg patche files

--- ports/librocket/portfile.cmake
+++ ports/librocket/portfile.cmake
@@ -1,24 +1,25 @@
 # 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
 
-set(LIBROCKET_VERSION 1.3.0.1)
+set(LIBROCKET_VERSION 1.3.0.0)
 vcpkg_download_distfile(ARCHIVE
-  URLS "https://github.com/i8degrees/libRocket/archive/refs/heads/release-${LIBROCKET_VERSION}.zip"
-  SHA512 b15714ebbe1ecf531ce8444a7bf750f4f702b23df76badf93a0124664d46c6c3e49070335afac3df38e0227b8dda17055d79b28bd08beaca78d74e422063ccad
+  URLS "https://github.com/libRocket/libRocket/archive/refs/tags/release-${LIBROCKET_VERSION}.zip"
+  SHA512 "c1de584f97963de770fc81b46cb0d2c30a00b3276c2f83549b2be8eab22ad381df83e0ff8d14c6208d370e26fc19c552078514f0a76bec4dd834dabb2c89b3c1"
   FILENAME "librocket-${LIBROCKET_VERSION}.zip"
 )
 
 vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH BASE_SOURCE_PATH
-    ARCHIVE "${ARCHIVE}"
-    REF 1.3.0.0
-    PATCHES
-      "001_fixup-shared-library-macosx.patch"
+  OUT_SOURCE_PATH BASE_SOURCE_PATH
+  ARCHIVE "${ARCHIVE}"
+  REF 1.3.0.0
+  PATCHES
+    001_backport-cmake-config.patch
+    002_remove-i386-osx-arch.patch
 )
 
 # FIXME: Feature selection is not yet supported
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-    python          "BUILD_PYTHON_BINDINGS=ON"
+  FEATURES
+  python          "BUILD_PYTHON_BINDINGS=ON"
 )
 
 vcpkg_cmake_configure(
@@ -42,8 +43,6 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/libRocket" "${CURRENT_PACKAGES_DIR}/lib/libRocket")
-
-#file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${BASE_SOURCE_PATH}/readme.md")
 
